7356fa29b17344c0dd4363da7df5598db4110e0a
Charles Colgan <cdcolgan@outlook.com>
2020-09-22 21:20:37 -0500

    General edits and added 1.8

--- Learn_REP2.R
+++ Learn_REP2.R
@@ -1,9 +1,14 @@
 #Libraryies and Data sets
 library(readr)
 library(class)
+library(ggplot2)
 VLADIMIR=read_csv("Documents/R/fonts/VLADIMIR.csv")
 EBRIMA=read_csv("Documents/R/fonts/EBRIMA.csv")
 BITSTREAMVERA=read_csv("Documents/R/fonts/BITSTREAMVERA.csv")
+
+#functions
+normalize=function(x) (x- min(x))/(max(x) - min(x))
+
 
 #Cleaning and sorting
 drop_names=c("fontVariant","m_label","orientation","m_top","m_left","originalH","originalW","h","w")
@@ -14,107 +19,146 @@ DATA=rbind(CL1,CL2,CL3)
 
 #(0)
 #mean and standard deviation
-m = apply(DATA[,-(1:3)],2,mean)
-s = apply(DATA[,-(1:3)],2,sd)
+m=apply(DATA[,-(1:3)],2,mean)
+s=apply(DATA[,-(1:3)],2,sd)
 
 #standardize
-CL1 = cbind(CL1[,1:3], t(t(sweep(CL1[,-(1:3)],2, m))/s))
-CL2 = cbind(CL2[,1:3], t(t(sweep(CL2[,-(1:3)],2, m))/s))
-CL3 = cbind(CL3[,1:3], t(t(sweep(CL3[,-(1:3)],2, m))/s))
-SDATA = rbind(CL1,CL2,CL3)
+CL1=cbind(CL1[,1:3],t(t(sweep(CL1[,-(1:3)],2,m))/s))
+CL2=cbind(CL2[,1:3],t(t(sweep(CL2[,-(1:3)],2,m))/s))
+CL3=cbind(CL3[,1:3],t(t(sweep(CL3[,-(1:3)],2,m))/s))
+DATA=rbind(CL1,CL2,CL3)
 
 #correlation matrix
-CORR = cor(DATA[,-(1:3)])
+CORR=cor(DATA[,-(1:3)])
 
 #finding highest correlation values
-up = upper.tri(CORR)
-out = data.frame(which(up, arr.ind=TRUE), cor=CORR[up])
-out = out[!is.na(out$cor),]
-out = out[order(abs(out$cor), decreasing=TRUE),]
-out$row = rownames(CORR)[out$row]
-out$col = colnames(CORR)[out$col]
-top10Cor = out[1:10,]
+up=upper.tri(CORR)
+out=data.frame(which(up, arr.ind=TRUE), cor=CORR[up])
+out=out[!is.na(out$cor),]
+out=out[order(abs(out$cor), decreasing=TRUE),]
+out$row=rownames(CORR)[out$row]
+out$col=colnames(CORR)[out$col]
+top10Cor=out[1:10,]
 
 #(1.0)
 set.seed(1) #make random selection reproducible
 #assign selections ~20% of each class
-r1 = sort(sample(nrow(CL1), nrow(CL1)*0.2))
-r2 = sort(sample(nrow(CL2), nrow(CL2)*0.2))
-r3 = sort(sample(nrow(CL3), nrow(CL3)*0.2))
+r1=sort(sample(nrow(CL1),nrow(CL1)*0.2))
+r2=sort(sample(nrow(CL2),nrow(CL2)*0.2))
+r3=sort(sample(nrow(CL3),nrow(CL3)*0.2))
 #define test&training sets
-testCL1 = CL1[r1,]; trainCL1 = CL1[-r1,]
-testCL2 = CL2[r2,]; trainCL2 = CL2[-r2,]
-testCL3 = CL3[r3,]; trainCL3 = CL3[-r3,]
+testCL1=CL1[r1,];trainCL1=CL1[-r1,]
+testCL2=CL2[r2,];trainCL2=CL2[-r2,]
+testCL3=CL3[r3,];trainCL3=CL3[-r3,]
 
-TRAINSET = rbind(trainCL1, trainCL2, trainCL3)
-TESTSET = rbind(testCL1, testCL2, testCL3)
+TRAINSET=rbind(trainCL1,trainCL2,trainCL3)
+TESTSET=rbind(testCL1,testCL2,testCL3)
 
 #(1.1)
 K=12
-#training set:
-train12 = knn(TRAINSET[,-(1:3)], TRAINSET[,-(1:3)], TRAINSET$font, K)
-trainperf12 = mean(train12 == TRAINSET$font)
-#test set:
-test12 = knn(TRAINSET[,-(1:3)], TESTSET[,-(1:3)], TRAINSET$font, K)
-testperf12 = mean(test12 == TESTSET$font) 
 
-#confusion matrices
-trainconf12 = table(TRAINSET$font, train12) #confusion matrix
-trainconf12/apply(trainconf12, 1, sum) #in percentages
-testconf12 = table(TESTSET$font, test12)
-testconf12/apply(testconf12, 1, sum)
+#train/test
+train12=knn(TRAINSET[,-(1:3)],TRAINSET[,-(1:3)],TRAINSET$font,K)
+test12=knn(TRAINSET[,-(1:3)],TESTSET[,-(1:3)],TRAINSET$font, K)
 
-#(1.2) 
-K = c(5,10,15,20,30,40,50,100)
-testperfK = NULL
+trainperf12=mean(train12==TRAINSET$font)
+testperf12=mean(test12==TESTSET$font)
+
+#(1.2) Determine best K by elbow method
+#takes a while to run
+K=c(5,10,15,20,30,40,50,100)
+testperfK=NULL
 for (i in K){ 
-  testknn = knn(TRAINSET[,-(1:3)], TESTSET[,-(1:3)], TRAINSET$font, i)
-  testperfK = c(testperfK, mean(testknn == TESTSET$font))
+  testknn=knn(TRAINSET[,-(1:3)],TESTSET[,-(1:3)],TRAINSET$font, i)
+  testperfK=c(testperfK,mean(testknn==TESTSET$font))
 }
-plot(K,testperfK, type="l")
 
-#(1.3)
-#lower values of K give better performance, try [a,b] = [1,5]
-K = c(1,2,3,4,5)
-testperfK = NULL
-for (i in K){ 
-  testknn = knn(TRAINSET[,-(1:3)], TESTSET[,-(1:3)], TRAINSET$font, i)
-  testperfK = c(testperfK, mean(testknn == TESTSET$font))
+Kval=as.data.frame(cbind(K,testperfK))
+
+#plot 0<=K<=100
+ggplot(Kval,aes(K,testperfK))+
+  geom_line()+
+  labs(x="K",y="% Correct classification on TESTSET",title="0<=K<=100")+
+  scale_x_continuous(breaks=round(seq(0,100,by=10),1))
+
+#(1.3) Closer inspection around K=100
+K_knee=c(95,96,97,98,99,100,101,102,103,104,105)
+testperfK_knee=NULL
+for (i in K_knee){ 
+  testknn_knee=knn(TRAINSET[,-(1:3)],TESTSET[,-(1:3)],TRAINSET$font,i)
+  testperfK_knee=c(testperfK_knee,mean(testknn_knee==TESTSET$font))
 }
-plot(K,testperfK, type="l") #K=1 gives best performance
+
+Kval_knee=as.data.frame(cbind(K_knee,testperfK_knee))
+
+#plot around K=101
+ggplot(Kval_knee,aes(K_knee,testperfK_knee))+
+  geom_line()+
+  labs(x="K",y="% Correct classification on TESTSET",title="Values around K=101")+
+  scale_x_continuous(breaks=round(seq(95,105,by=1),1))
 
 #(1.4)
-#training set:
-train1 = knn(TRAINSET[,-(1:3)], TRAINSET[,-(1:3)], TRAINSET$font, 1)
-trainperf1 = mean(train1 == TRAINSET$font)
-#test set:
-test1 = knn(TRAINSET[,-(1:3)], TESTSET[,-(1:3)], TRAINSET$font, 1)
-testperf1 = mean(test1 == TESTSET$font) 
+Kbest=101
 
-#confusion matrices
-trainconf1 = table(TRAINSET$font, train1) #confusion matrix
-trainconf1/apply(trainconf1, 1, sum) #in percentages
-testconf1 = table(TESTSET$font, test1)
-testconf1/apply(testconf1, 1, sum)
+#train/test
+train101=knn(TRAINSET[,-(1:3)],TRAINSET[,-(1:3)],TRAINSET$font,Kbest)
+test101=knn(TRAINSET[,-(1:3)],TESTSET[,-(1:3)],TRAINSET$font,Kbest)
 
-#(1.6)
-PACK1 = NULL
+trainperf101=mean(train101==TRAINSET$font)
+testperf101=mean(test101==TESTSET$font)
+
+#confusion matrices 
+trainconf101=table(TRAINSET$font,train101)
+testconf101=table(TESTSET$font,test101)
+
+#conf in %'s
+trainconf101/apply(trainconf101,1,sum)
+testconf101/apply(testconf101,1,sum)
+
+#(1.5)
+
+#(1.6,1.7) Binning by groups of pixels
+PACK1=NULL;PACK2=NULL;PACK3=NULL;PACK4=NULL;
+
 for(L in 0:9)  for(M in 0:9)  PACK1 = c(PACK1, sprintf('r%ic%i',L,M))
+for(L in 0:9)  for(M in 10:19)  PACK2=c(PACK2,sprintf('r%ic%i',L,M))
+for(L in 10:19)  for(M in 10:19)  PACK3=c(PACK3,sprintf('r%ic%i',L,M))
+for(L in 10:19)  for(M in 0:9)  PACK4=c(PACK4,sprintf('r%ic%i',L,M))
 
-testknnP1 = knn(TRAINSET[,PACK1], TESTSET[,PACK1], TRAINSET$font, 1)
-w1 = mean(testknnP1 == TESTSET$font)
+P1KN=knn(TRAINSET[,PACK1],TESTSET[,PACK1],TRAINSET$font,101)
+P2KN=knn(TRAINSET[,PACK2],TESTSET[,PACK2],TRAINSET$font,101)
+P3KN=knn(TRAINSET[,PACK3],TESTSET[,PACK3],TRAINSET$font,101)
+P4KN=knn(TRAINSET[,PACK4],TESTSET[,PACK4],TRAINSET$font,101)
 
-#(1.7)
-PACK2=NULL; PACK3=NULL; PACK4=NULL;
-for(L in 0:9)  for(M in 10:19)  PACK2 = c(PACK2, sprintf('r%ic%i',L,M))
-for(L in 10:19)  for(M in 10:19)  PACK3 = c(PACK3, sprintf('r%ic%i',L,M))
-for(L in 10:19)  for(M in 0:9)  PACK4 = c(PACK4, sprintf('r%ic%i',L,M))
+w1=mean(P1KN==TESTSET$font)
+w2=mean(P2KN==TESTSET$font)
+w3=mean(P3KN==TESTSET$font)
+w4=mean(P4KN==TESTSET$font)
 
-testknnP2 = knn(TRAINSET[,PACK2], TESTSET[,PACK2], TRAINSET$font, 1)
-w2 = mean(testknnP2 == TESTSET$font)
+#(1.8)Weigthing and normalization
+COM=w1*TESTSET[,PACK1]+w2*TESTSET[,PACK2]+w3*TESTSET[,PACK3]+w4*TESTSET[,PACK4]
+W_TRAINSET=cbind(w1*TRAINSET[,PACK1]/sum(COM),
+                 w2*TRAINSET[,PACK2]/sum(COM),
+                 w3*TRAINSET[,PACK3]/sum(COM),
+                 w4*TRAINSET[,PACK4]/sum(COM))
+W_TESTSET=cbind(w1*TESTSET[,PACK1]/sum(COM),
+                w2*TESTSET[,PACK2]/sum(COM),
+                w3*TESTSET[,PACK3]/sum(COM),
+                w4*TESTSET[,PACK4]/sum(COM))
 
-testknnP3 = knn(TRAINSET[,PACK3], TESTSET[,PACK3], TRAINSET$font, 1)
-w3 = mean(testknnP3 == TESTSET$font)
+#KNN on normalized sets
+W_train101=knn(W_TRAINSET,W_TRAINSET,TRAINSET$font,Kbest)
+W_test101=knn(W_TRAINSET,W_TESTSET,TRAINSET$font,Kbest)
 
-testknnP4 = knn(TRAINSET[,PACK4], TESTSET[,PACK4], TRAINSET$font, 1)
-w4 = mean(testknnP4 == TESTSET$font)
+W_trainperf101=mean(W_train101==TRAINSET$font)
+W_testperf101=mean(W_test101==TESTSET$font)
+
+#confusion matrix
+W_trainconf101=table(TRAINSET$font,W_train101)
+W_testconf101=table(TESTSET$font,W_test101)
+
+#conf in %'s
+W_trainconf101/apply(trainconf101,1,sum) 
+W_testconf101/apply(testconf101,1,sum)
+
+
